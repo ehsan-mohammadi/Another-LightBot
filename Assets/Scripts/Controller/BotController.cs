bfd1b382bd1c536204301a96c4c8b153bbc08340
ehsan-mohammadi <mohammadi.ehsan1994@gmail.com>
2022-05-08 22:50:56 +0430

    Add BotController

---
+++ Assets/Scripts/Controller/BotController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Game.Controller
+{
+    using Model;
+
+    public class BotController : MonoBehaviour
+    {
+        #region Variables
+            [SerializeField]
+            private float speed = 5f;
+
+            [SerializeField]
+            private Position currentPosition;
+
+            [SerializeField]
+            private Direction currentDirection = Direction.FORWARD;
+
+            private enum Direction { FORWARD, BACKWARD, LEFT, RIGHT }
+        #endregion
+
+        #region Methods
+            private void Update ()
+            {
+                
+            }
+
+            private void Move ()
+            {
+
+            }
+        #endregion
+    }
+}
