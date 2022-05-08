63c238ecbe903e89318dbcc485595e36a3c084dd
ehsan-mohammadi <mohammadi.ehsan1994@gmail.com>
2022-05-08 13:56:52 +0430

    Initial commit

---
+++ Assets/Scripts/Model/Platform.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Game.Model
+{
+    public class Platform : MonoBehaviour
+    {
+        #region Variables
+            [SerializeField]
+            protected int height = 1;
+
+            [SerializeField]
+            private GameObject block;
+            
+            protected static readonly Vector3 offset = new Vector3(0, 0.5f, 0);
+        #endregion
+
+        #region SetterGetters
+            public int Height
+            {
+                get { return height; }
+                set { height = value; }
+            }
+        #endregion
+
+        #region Methods
+            public void Awake ()
+            {
+                GenerateBlockStack();
+            }
+
+            private void GenerateBlockStack ()
+            {
+                for (int i = 0; i < height; i++)
+                {
+                    Instantiate(block
+                        , this.transform.position + (offset * i)
+                        , Quaternion.identity
+                        , this.transform);
+                }
+            }
+
+            private void OnDrawGizmos ()
+            {
+                for (int i = 0; i < height; i++)
+                    Gizmos.DrawWireCube(this.transform.position + (offset * i)
+                        , Vector3.one - offset);
+            }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Model/TargetPlatform.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Game.Model
+{
+    public class TargetPlatform : Platform
+    {
+        #region Variables
+            [SerializeField]
+            private Material switchOff;
+
+            [SerializeField]
+            private Material switchOn;
+        #endregion
+
+        #region Methods
+            private void OnDrawGizmos ()
+            {
+                Gizmos.color = Color.cyan;
+                for (int i = 0; i < height; i++)
+                    Gizmos.DrawWireCube(this.transform.position + (offset * i)
+                        , Vector3.one - offset);
+            }
+        #endregion
+    }
+}
