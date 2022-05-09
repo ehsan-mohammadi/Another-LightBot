aabf0d21ab3bd5636b4605616d97720ffe878db5
ehsan-mohammadi <mohammadi.ehsan1994@gmail.com>
2022-05-09 10:06:41 +0430

    Add UIHandler and OperationClickHandler

---
+++ Assets/Scripts/UI/OperationClickHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Game.UI
+{
+    public class OperationClickHandler : MonoBehaviour
+    {
+        #region Variables
+            private bool isAdded = false;
+        #endregion
+
+        #region SetterGetters
+            internal bool IsAdded
+            {
+                get { return isAdded; }
+                set { isAdded = value; }
+            }
+        #endregion
+
+        #region Methods
+            public void OnButtonClick ()
+            {
+                if (!isAdded)
+                    UIHandler.Instance.AddOperation(this);
+                else
+                    UIHandler.Instance.RemoveOperation(this);
+            }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/UI/UIHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Game.UI
+{
+    public class UIHandler : MonoBehaviour
+    {
+        #region Variables
+            [SerializeField]
+            private Transform mainProc;
+
+            public static UIHandler Instance;
+        #endregion
+
+        #region Methods
+            private void Awake ()
+            {
+                if (Instance != null)
+                    Destroy(this.gameObject);
+                Instance = this;
+            }
+
+            internal void AddOperation (OperationClickHandler operation)
+            {
+                OperationClickHandler instance = Instantiate(operation
+                    , Vector3.zero, Quaternion.identity, mainProc);
+                instance.IsAdded = true;
+            }
+
+            internal void RemoveOperation (OperationClickHandler operation)
+            {
+                Destroy(operation.gameObject);
+            }
+        #endregion
+    }
+}
